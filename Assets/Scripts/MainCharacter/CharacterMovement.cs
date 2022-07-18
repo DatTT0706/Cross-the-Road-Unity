2580a673b88cb399edc445e7a48f65c818a759d8
dngoc <Dung.CaoNgoc@ingenico.com>
2022-07-18 22:55:27 +0700

    Modify movement when game start

--- Assets/Scripts/MainCharacter/CharacterMovement.cs
+++ Assets/Scripts/MainCharacter/CharacterMovement.cs
@@ -5,30 +5,43 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     Vector3 Vec;
+    private StateChangeListener mListener;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameController.instance.isPlaying)
+        {
+            GetPlayerInput();
+        }
+    }
+
+    public void GetPlayerInput()
+    {
         Vec = transform.position;
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
         {
             Vec.y += 5;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             Vec.x -= 5;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             Vec.x += 5;
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            Vec.y -= 5;
+        }
         //Vec.x += Input.GetKeyDown("Horizontal") * Time.deltaTime * 60;
-        
-        transform.position = Vector3.MoveTowards(transform.position ,Vec, 1);
+
+        transform.position = Vector3.MoveTowards(transform.position, Vec, 1);
     }
 }
